e60c492fb819d9615d617c4bfb23e49da991ee05
rosales-stephanie <sr4581@nyu.edu>
2019-12-02 22:27:45 -0500

    Add files via upload

---
+++ Vampire Slayer Pt 2/Entity.h
@@ -0,0 +1,46 @@
+#pragma once
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "Mesh.h"
+enum EntityType { NONE, FLOOR, BOX, ENEMY };
+class Entity {
+public:
+    EntityType entityType;
+    bool billboard;
+    float width;
+    float height;
+    float depth;
+    bool CheckCollision(Entity *other);
+    
+    glm::vec3 position;
+    glm::vec3 velocity;
+    glm::vec3 acceleration;
+    glm::vec3 rotation;
+    glm::vec3 scale;
+    
+    Mesh* mesh;
+    
+    float *vertices;
+    float *texCoords;
+    int numVertices;
+    
+    GLuint textureID;
+    
+    Entity();
+    void Update(float deltaTime, Entity *player, Entity *objects, int objectCount);
+    //void Update(float deltaTime);
+    void Render(ShaderProgram *program);
+    void DrawBillboard(ShaderProgram *program);
+};
+
+
+
